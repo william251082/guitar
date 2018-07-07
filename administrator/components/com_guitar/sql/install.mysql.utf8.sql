e99ccad6ff284aa20a9dd4e58e23e88cf5349447
William del Rosario <williamdelrosario@yahoo.com>
2018-07-07 08:53:04 +0200

    Added database to component com_guitar

---
+++ administrator/components/com_guitar/sql/install.mysql.utf8.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `ymfgb__guitar_styles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) NOT NULL DEFAULT '',
+  `style` text NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+
+INSERT IGNORE INTO `ymfgb__guitar_styles` (`id`,`title`,`style`) VALUES
+	(1, 'Jazz', '1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+	(2, 'Classical', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
---
+++ administrator/components/com_guitar/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS `ymfgb__guitar_styles`;
