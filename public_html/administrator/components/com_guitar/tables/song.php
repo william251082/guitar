aef4943fe4641bbe97afb18c5179b60c8a6925ff
William del Rosario <williamdelrosario@yahoo.com>
2018-07-15 00:03:50 +0200

    Adding statndard fields to component - BUG

--- public_html/administrator/components/com_guitar/tables/song.php
+++ public_html/administrator/components/com_guitar/tables/song.php
@@ -119,6 +119,31 @@ class GuitarTableSong extends JTable
 		return $rules;
 	}
 
+    public function store($updateNulls = false)
+    {
+        $date = JFactory::getDate();
+        $user = JFactory::getUser();
+        if (!$this->id) {
+            if(!(int)$this->created) {
+                $this->created = $date->toSql();
+            }
+            if (empty($this->created_by)) {
+                $this->created_by = $user->get('id');
+            }
+        }
+        // Set publish up to null date if not set
+        if(!$this->publish_up) {
+            $this->publish_up = $this->_db->getNullDate();
+        }
+        // Set publish down to null date if not set
+        if(!$this->publish_down) {
+            $this->publish_down = $this->_db->getNullDate();
+        }
+
+        // Attempt to store the user data.
+        return parent::store($updateNulls);
+    }
+
 	/**
 	 * Overloaded check function
 	 *
@@ -131,6 +156,51 @@ class GuitarTableSong extends JTable
 		{
 			$this->ordering = self::getNextOrder();
 		}
+
+        // check for valid name
+        if (trim($this->song_title) == '') {
+            $this->setError(JText::_('COM_GUITAR_ERR_TABLES_ALBUM'));
+
+            return false;
+        }
+        $this->alias = JApplication::stringURLSafe($this->alias);
+        if (empty($this->alias)) {
+            $this->alias = JApplication::stringURLSafe($this->album);
+        }
+        // Check if the category is set
+        if (trim($this->catid) == '') {
+            $this->setError(JText::_('COM_CONTACT_WARNING_CATEGORY'));
+
+            return false;
+        }
+        // Cleanup the metakeywords
+        // remove the extra space and cr (\r) and if (\n) characters from the string
+        if (!empty($this->metakey)) {
+            // only process if not empty
+            $bad_characters = array("\n", "\r", "\"", "<", ">",); // array of characters to remove
+            $after_clean = JString::str_ireplace($bad_characters, "", $this->metakey); // remove bad characters
+            $keys = explode(',', $after_clean); // create array using commas as delimiter
+            $clean_keys = array();
+            foreach ($keys as $key) {
+                if (trim($key)) //ignore blank keywords
+                    $clean_keys[] = trim($key);
+            }
+        }
+        $this->metakey = implode(", ", $clean_keys); // put array back together delimited by ", "
+
+        // remove quotes and <> brackets
+        if (!empty($this->metadesc)) {
+            // only process if not empty
+            $bad_characters = array("\"", "<", ">");
+            $this->metadesc = JString::str_ireplace($bad_characters, "", $this->metadesc);
+        }
+        // check if the publish down data is not earlier than the publish up
+        if ((int)$this->publish_down > 0 && $this->publish_down < $this->publish_up) {
+            $this->setError(JText::_('JGLOBAL_START_PUBLISH_AFTER_FINISH'));
+
+            return false;
+        }
+
 		return parent::check();
 	}
 
--- public_html/administrator/components/com_guitar/views/song/tmpl/edit.php
+++ public_html/administrator/components/com_guitar/views/song/tmpl/edit.php
@@ -48,14 +48,33 @@ $document->addStyleSheet(JUri::root() . 'media/com_guitar/css/form.css');
 
 	<div class="form-horizontal">
 		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'general')); ?>
-
 		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'general', JText::_('COM_GUITAR_TITLE_SONG', true)); ?>
 		<div class="row-fluid">
 			<div class="span10 form-horizontal">
+
 				<fieldset class="adminform">
+                    <ul class="nav nav-tabs">
+                        <li class="active">
+                            <a href="#details" data-toggle="tab"><?php echo empty($this->item->id) ? JText::_(
+                                    'COM_GUITAR_NEW_SONG') : JText::sprintf('COM_GUITAR_EDIT_SONG', $this->item->id
+                                ); ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#metadata" data-toggle="tab"><?php echo JText::_('JGLOBAL_FIELDSET_METADATA_OPTIONS');?>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#publishingoptions" data-toggle="tab"><?php echo JText::_(
+                                    'JGLOBAL_FIELDSET_PUBLISHING'
+                                );?>
+                            </a>
+                        </li>
+                    </ul>
 
-									<input type="hidden" name="jform[id]" value="<?php echo $this->item->id; ?>" />
-
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="details">
+                            <input type="hidden" name="jform[id]" value="<?php echo $this->item->id; ?>" />
 <!--					--><?php //if ($this->state->params->get('save_history', 1)) : ?>
 <!--					    <div class="control-group">-->
 <!--					    	<div class="control-label">--><?php //echo $this->form->getLabel('version_note'); ?><!--</div>-->
@@ -72,6 +91,11 @@ $document->addStyleSheet(JUri::root() . 'media/com_guitar/css/form.css');
                         </div>
 
                         <div class="control-group">
+                            <div class="control-label"><?php echo $this->form->getLabel('alias'); ?></div>
+                            <div class="controls"><?php echo $this->form->getInput('alias'); ?></div>
+                        </div>
+
+                        <div class="control-group">
                             <div class="control-label"><?php echo $this->form->getLabel('song_info'); ?></div>
                             <div class="controls"><?php echo $this->form->getInput('song_info'); ?></div>
                         </div>
@@ -81,7 +105,26 @@ $document->addStyleSheet(JUri::root() . 'media/com_guitar/css/form.css');
                             <div class="controls"><?php echo $this->form->getInput('catid'); ?></div>
                         </div>
 
+                            <div class="control-group">
+                                <div class="control-label"><?php echo $this->form->getLabel('id'); ?></div>
+                                <div class="controls"><?php echo $this->form->getInput('id'); ?></div>
+                            </div>
+
+                            <div class="tab-pane" id="metadata">
+                                <fieldset>
+                                    <?php echo $this->loadTemplate('metadata'); ?>
+                                </fieldset>
+                            </div>
+                            <div class="tab-pane" id="publishingoptions">
+                                <fieldset>
+                                    <?php echo $this->loadTemplate('publishingoptions'); ?>
+                                </fieldset>
+                            </div>
+
 <!--					--><?php //endif; ?>
+
+                        </div>
+                    </div>
 				</fieldset>
 			</div>
 		</div>
@@ -98,5 +141,26 @@ $document->addStyleSheet(JUri::root() . 'media/com_guitar/css/form.css');
 		<input type="hidden" name="task" value=""/>
 		<?php echo JHtml::_('form.token'); ?>
 
+        <!-- Begin Sidebar -->
+        <div class="span2">
+            <h4><?php echo JText::_('JDETAILS');?></h4>
+            <hr />
+            <fieldset class="form-vertical">
+                <div class="control-group">
+                    <div class="control-group">
+                        <div class="controls">
+                            <?php echo $this->form->getValue('name'); ?>
+                        </div>
+                    </div>
+                    <div class="control-label">
+                        <?php echo $this->form->getLabel('published'); ?>
+                    </div>
+                    <div class="controls">
+                        <?php echo $this->form->getInput('published'); ?>
+                    </div>
+                </div>
+            </fieldset>
+        </div>
+        <!-- End Sidebar -->
 	</div>
 </form>
